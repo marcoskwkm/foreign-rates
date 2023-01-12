c5a146b714234eca8a210ae3c59a4d62c099905f
Marcos Kawakami <marcoskwkm@gmail.com>
2023-01-12 00:10:40 -0300

    Prepare for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -36,14 +36,28 @@ const App = () => {
 
   return (
     <div className="h-screen overflow-y-auto bg-gray-900 p-8 text-gray-300">
-      <div className="flex flex-col gap-y-2">
-        {rates.map((rate) => (
-          <ExchangeRate key={rate.source + rate.target} rate={rate} />
-        ))}
-      </div>
-      <div className="mt-2 text-sm">
-        Last updated: {new Date(lastUpdated).toLocaleTimeString()}
-      </div>
+      {convs.length === 0 ? (
+        <>
+          <div>No rates to display. Add some via query string.</div>
+          <div>Example: {window.location.host}/?from=usd&to=brl</div>
+        </>
+      ) : (
+        <>
+          <div className="flex flex-col gap-y-2">
+            {rates.map((rate) => (
+              <ExchangeRate key={rate.source + rate.target} rate={rate} />
+            ))}
+          </div>
+          <div className="mt-2 text-sm">
+            Last updated:{' '}
+            {new Date(lastUpdated).toLocaleTimeString('pt-BR', {
+              year: '2-digit',
+              month: '2-digit',
+              day: '2-digit',
+            })}
+          </div>
+        </>
+      )}
     </div>
   )
 }
